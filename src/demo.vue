339d878e886c5d24b9e812e4b24b7373392f7077
wanglifa <wanglifa1995@gmail.com>
2019-08-09 16:11:25 +0800

    初始化项目单元测试通过

--- src/demo.vue
+++ src/demo.vue
@@ -1,10 +1,16 @@
 <template>
-  
+  <div>
+    <lf-button icon="left">测试</lf-button>
+  </div>
 </template>
 
 <script>
+  import LfButton from './button'
   export default {
-    name: "demo"
+    name: "demo",
+    components: {
+      LfButton
+    }
   }
 </script>
 
---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg class="lf-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+<script>
+    import './svg.js'
+    export default {
+        name: 'LiFaIcon',
+        props: ['name']
+    }
+</script>
+<style lang="scss" scoped>
+    .lf-icon{
+        width: 1em;
+        height: 1em;
+    }
+</style>
